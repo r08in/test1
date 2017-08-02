dcfd1ad8e5ee717f061b769ce948cbd4f68b0030
r08in <r08bin@163.com>
2017-08-02 16:31:39 -0400

    frist change.

---
+++ t2.py
@@ -0,0 +1,13 @@
+'''
+Created on Aug 2, 2017
+
+@author: Administrator
+'''
+print("Hello, Python!")
+counter = 120          # An integer assignment
+miles   = 1000.0       # A floating point
+name    = "John"       # A string
+
+print(counter)
+print(miles)
+print(name)
