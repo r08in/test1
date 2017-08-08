b5ea71465c4a413af613a263295299ae68a2a34c
B_LUO <B_LUO@BROWN327-01.campus.uncg.edu>
2017-08-08 10:06:51 -0400

    new try

--- t2.py
+++ t2.py
@@ -6,7 +6,7 @@ Created on Aug 2, 2017
 print("Hello, Python!")
 counter = 120          # An integer assignment
 miles   = 1000.0       # A floating point
-name    = "John"       # A string
+name    = "Bin"       # A string
 
 print(counter)
 print(miles)
